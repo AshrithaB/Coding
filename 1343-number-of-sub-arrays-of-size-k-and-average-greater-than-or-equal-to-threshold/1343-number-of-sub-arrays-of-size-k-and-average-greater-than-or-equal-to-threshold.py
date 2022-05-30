aef3a54d678c58f9e516617f967215b89f4c650a
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-05-30 15:55:05 +0530

    Time: 729 ms (61.50%), Space: 27 MB (78.02%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,6 +1,17 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        subarr, curavg = 0, 0
+        subarr = 0
+        curavg = sum(arr[:k])
+        if curavg/k >= threshold:
+            subarr += 1
+        l = 0
+        for r in range(k, len(arr)):
+            curavg += arr[r] - arr[l]
+            l += 1
+            if curavg/k >= threshold:
+                subarr += 1
+        return subarr
+        '''subarr, curavg = 0, 0
         for i in range(k-1):
             curavg += arr[i]
         for r in range(k-1, len(arr)):
@@ -8,4 +19,4 @@ class Solution:
             if curavg/k >= threshold:
                 subarr += 1
             curavg -= arr[r-k+1]
-        return subarr
+        return subarr'''
