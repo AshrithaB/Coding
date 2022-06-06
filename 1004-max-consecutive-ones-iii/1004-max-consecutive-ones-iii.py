b91fb2d768bbfcad2dc4ea5d37911772930e6a5b
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-06 18:09:28 +0530

    Time: 1116 ms (13.19%), Space: 14.7 MB (59.76%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -5,10 +5,10 @@ class Solution:
         for r in range(len(nums)):
             if nums[r] == 0:
                 seen += 1
-            while seen > k:
-                if nums[l] == 0:
-                    seen -= 1
-                l += 1
+                while seen > k:
+                    if nums[l] == 0:
+                        seen -= 1
+                    l += 1
             maxLen = max(maxLen, r-l+1)
         return maxLen
         
