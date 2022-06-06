2d837e6fe13fb83a3c296ce0c58b6b73352ebf58
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-06 18:24:19 +0530

    Time: 7526 ms (5.03%), Space: 15.8 MB (74.57%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.py
+++ 480-sliding-window-median/480-sliding-window-median.py
@@ -1,12 +1,13 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        res = []
+        median = []
         l = 0
         for r in range(k, len(nums)+1):
-            if k%2 == 0: 
-                res.append((sorted(nums[l:r])[(k//2)-1] + sorted(nums[l:r])[k//2])/2)
+            arr = sorted(nums[l:r])
+            if k%2 == 0:
+                median.append((arr[k//2] + arr[(k//2)-1])/2)
             else:
-                res.append(sorted(nums[l:r])[k//2])
+                median.append(arr[k//2])
             l += 1
-        return res
+        return median
         
