85e32c02e05f58dbd0734707c77f9dce45961bb3
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-07 17:09:36 +0530

    Time: 122 ms (52.29%), Space: 14 MB (68.02%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,19 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
-        s1Count, s2Count = {}, {}
+        s1Count, s2Count = [0]*26, [0]*26
         for i in range(len(s1)):
-            s1Count[s1[i]] = 1 + s1Count.get(s1[i], 0)
-            s2Count[s2[i]] = 1 + s2Count.get(s2[i], 0)
-        if s1Count == s2Count:
-            return True
+            s1Count[ord(s1[i])-ord('a')] += 1
+            s2Count[ord(s2[i])-ord('a')] += 1
+        match = 0
+        for j in range(26):
+            match += 1 if s1Count[j]==s2Count[j] else 0
         l = 0
         for r in range(len(s1), len(s2)):
-            s2Count[s2[r]] = 1 + s2Count.get(s2[r], 0)
-            s2Count[s2[l]] -= 1
-            if s2Count[s2[l]] == 0:
-                del s2Count[s2[l]]
-            l += 1
-            if s1Count == s2Count:
+            if match == 26: 
                 return True
-        return False
+            index = ord(s2[r])-ord('a')
+            s2Count[index] += 1
+            if s2Count[index] == s1Count[index]:
+                match += 1
+            elif s2Count[index] == s1Count[index]+1:
+                match -= 1
+            index = ord(s2[l])-ord('a')
+            s2Count[index] -= 1
+            if s2Count[index] == s1Count[index]:
+                match += 1
+            elif s2Count[index]+1 == s1Count[index]:
+                match -= 1
+            l += 1
+        return False if match != 26 else True
+        
