24fd8703eb9c7a1f16ef421145584c612953a643
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-07 16:26:43 +0530

    Time: 197 ms (51.64%), Space: 15.2 MB (78.83%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,30 +1,20 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s): return []
-        sCount, pCount = [0]*26, [0]*26
-        indices = []
-        for j in range(len(p)):
-            pCount[ord(p[j]) - ord('a')] += 1
-            sCount[ord(s[j]) - ord('a')] += 1
-        matches = 0    
-        for i in range(26):
-            matches += 1 if sCount[i] == pCount[i] else 0
+        if len(p) > len(s): return []
+        sCount, pCount = {}, {}
+        for i in range(len(p)):
+            pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+        res = [0] if pCount == sCount else []
         l = 0
         for r in range(len(p), len(s)):
-            if matches == 26: indices.append(l)
-            index = ord(s[r]) - ord('a')
-            sCount[index] += 1
-            if sCount[index] == pCount[index]:
-                matches += 1
-            elif pCount[index] + 1 == sCount[index]:
-                matches -= 1
-            index = ord(s[l]) - ord('a')
-            sCount[index] -= 1
-            if sCount[index] == pCount[index]:
-                matches += 1
-            elif pCount[index] - 1 == sCount[index]:
-                matches -= 1
+            sCount[s[l]] -= 1
+            if sCount[s[l]] == 0:
+                del sCount[s[l]]
             l += 1
-        if matches == 26: indices.append(l)
-        return indices
+            sCount[s[r]] = 1 + sCount.get(s[r], 0)
+            if sCount == pCount:
+                res.append(l)
+        return res
+
         
