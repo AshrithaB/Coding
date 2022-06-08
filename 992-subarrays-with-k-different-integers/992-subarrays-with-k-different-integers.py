c9ab1487930875d84376cbcd94b33b6ae11ed5b6
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-08 15:20:24 +0530

    Time: 917 ms (16.99%), Space: 16.6 MB (80.06%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
@@ -1,6 +1,6 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        def subarrayWithAtmostK(K: int) -> int:
+        def subarryWithAtmostK(K):
             seen = {}
             ans = 0
             l = 0
@@ -8,11 +8,10 @@ class Solution:
                 seen[nums[r]] = 1 + seen.get(nums[r], 0)
                 while len(seen) > K:
                     seen[nums[l]] -= 1
-                    if seen[nums[l]]==0:
-                        del(seen[nums[l]])
+                    if seen[nums[l]] == 0:
+                        seen.pop(nums[l])
                     l += 1
                 ans += r-l+1
             return ans
-        return subarrayWithAtmostK(k) - subarrayWithAtmostK(k-1)       
-            
+        return subarryWithAtmostK(k) - subarryWithAtmostK(k-1)
         
