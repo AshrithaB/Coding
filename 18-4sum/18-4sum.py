c48c158d738b45f86e4301721c453f1abdd44a26
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-06-28 18:19:56 +0530

    Time: 882 ms (66.95%), Space: 14.2 MB (17.77%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -5,7 +5,7 @@ class Solution:
         def kSum(k, start, target):
             if k != 2:
                 for i in range(start, len(nums)-k+1):
-                    if i>start and nums[i] == nums[i-1]:
+                    if i > start and nums[i] == nums[i-1]:
                         continue
                     quad.append(nums[i])
                     kSum(k-1, i+1, target-nums[i])
@@ -13,16 +13,16 @@ class Solution:
                 return
             l, r = start, len(nums)-1
             while l<r:
-                curSum = nums[l]+nums[r]
+                curSum = nums[l] + nums[r] 
                 if curSum < target:
                     l += 1
                 elif curSum > target:
                     r -= 1
                 else:
-                    res.append(quad + [nums[l], nums[r]])
+                    res.append(quad + [nums[l],nums[r]])
                     l += 1
-                    while l<r and nums[l] == nums[l-1]:
+                    while l<r and nums[l-1] == nums[l]:
                         l += 1
             return
-        kSum(4, 0, target)                    
+        kSum(4, 0, target)
         return res
