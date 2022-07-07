5efc44300b61d77e96f44eb4f5279f7c9697d60c
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-07 15:50:11 +0530

    Time: 510 ms (6.17%), Space: 17.8 MB (80.51%) - LeetHub

---
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        if not head: return None        
+        m = self.mid(head)
+        root = TreeNode(m.val)
+        if head == m:
+            return root
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(m.next)
+        return root
+     
+    def mid(self, head):
+        slow = fast = prev = head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        if prev:
+            prev.next = None
+        return slow       
+        
+
+        
+        
