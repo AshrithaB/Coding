335a87a0958cc3ccf9407b191c24d74a77ecfd7f
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-14 16:34:58 +0530

    Time: 84 ms (71.57%), Space: 13.9 MB (92.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLen = 0
         seen = {}
+        maxLen = 0
         l = 0
         for r in range(len(s)):
             seen[s[r]] = 1 + seen.get(s[r], 0)
@@ -9,7 +9,7 @@ class Solution:
                 seen[s[l]] -= 1
                 if seen[s[l]] == 0:
                     del seen[s[l]]
-                l += 1                
+                l += 1
             maxLen = max(maxLen, r-l+1)
         return maxLen
-        
+            
