9bd528a68d21fed60c26f8432d03cd39f56f0b06
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-16 12:41:00 +0530

    Time: 160 ms (55.56%), Space: 14.7 MB (82.83%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,11 +1,12 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "": return ""
+        if not t:
+            return ""
         window, countT = {}, {}
-        for ch in t:
-            countT[ch] = 1 + countT.get(ch, 0)
+        for w in t:
+            countT[w] = 1 + countT.get(w, 0)
         have, need = 0, len(countT)
-        res, resLen = [-1,-1], float("infinity")
+        res, reslength = [-1, -1], float('infinity')
         l = 0
         for r in range(len(s)):
             ch = s[r]
@@ -13,16 +14,16 @@ class Solution:
             if ch in countT and window[ch] == countT[ch]:
                 have += 1
             while have == need:
-                if r-l+1 < resLen:
+                if r-l+1 < reslength:
                     res = [l, r]
-                    resLen = r-l+1
+                    reslength = r-l+1
                 window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l:r+1] if resLen != float('infinity') else ""
-                
+        return s[l:r+1] if reslength != float('infinity') else ""
+        
                 
             
         
