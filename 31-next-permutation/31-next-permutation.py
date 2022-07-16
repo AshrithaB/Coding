f4137cc704169032b5220e720385d07a5bc32933
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-16 12:15:44 +0530

    Time: 75 ms (29.97%), Space: 14 MB (24.65%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,22 +1,23 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
         if len(nums) == 1:
-            return
-        lastind = -1
-        i = 1
-        while i<len(nums):
+            return nums
+        lastPeakInd = -1
+        for i in range(1, len(nums)):
             if nums[i] > nums[i-1]:
-                lastind = i
-            i += 1
-        if lastind == -1:
+                lastPeakInd = i
+        if lastPeakInd == -1:
             for k in range(len(nums)//2):
                 nums[k], nums[len(nums)-k-1] = nums[len(nums)-k-1], nums[k]
             return
-        index = lastind
-        for j in range(lastind+1, len(nums)):
-            if nums[j]>nums[lastind-1] and nums[j]<nums[index]:
-                index = j
-        nums[index], nums[lastind-1] = nums[lastind-1], nums[index]
-        nums[lastind:] = sorted(nums[lastind:])
+        peakNum = nums[lastPeakInd]
+        index = lastPeakInd
+        for i in range(lastPeakInd+1, len(nums)):
+            if nums[i]>nums[lastPeakInd-1] and nums[i]<nums[index]:
+                index = i
+        nums[lastPeakInd-1], nums[index] = nums[index], nums[lastPeakInd-1]
+        nums[lastPeakInd:] = sorted(nums[lastPeakInd:])   
         return
+            
         
