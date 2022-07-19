72e2193302032089ab45aef1fd44022a5febafe2
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-19 11:40:50 +0530

    Time: 72 ms (13.08%), Space: 13.8 MB (60.10%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def next_n(n):
-            total_sum = 0
-            while n > 0:
-                rem = n % 10
-                total_sum += rem*rem
-                n = n//10
-            return total_sum
+        def getn(num):
+            res = 0
+            while num:
+                rem = num%10
+                res = res + (rem*rem)
+                num = num//10
+            return res
+            
+            
         slow = n
-        fast = next_n(n)
-        while fast != 1 and slow != fast:
-            slow = next_n(slow)
-            fast = next_n(next_n(fast))
+        fast = getn(n)
+        while slow != fast and fast != 1:
+            slow = getn(slow)
+            fast = getn(getn(fast))
         return fast == 1
