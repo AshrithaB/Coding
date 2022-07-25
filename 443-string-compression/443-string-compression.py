c55accb644e76213530222b2272662c48e33b610
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-25 10:39:04 +0530

    Time: 99 ms (38.64%), Space: 14.1 MB (34.51%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,20 +1,18 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         if len(chars) == 1: return 1
-        start, end, index = 0, 0, 0
+        start = index = end = 0
         while end < len(chars):
             end = start
-            while end<len(chars) and chars[start] == chars[end]:
-                end += 1
+            while end < len(chars) and chars[start] == chars[end]:
+                end = end + 1
             chars[index] = chars[start]
             index += 1
-            if end-start > 1:
-                num = list(str(end-start))
-                for x in num:
-                    chars[index] = x
+            l = end-start
+            if l > 1:
+                l = str(l)
+                for c in l:
+                    chars[index] = c
                     index += 1
             start = end
         return index
-            
-            
-        
