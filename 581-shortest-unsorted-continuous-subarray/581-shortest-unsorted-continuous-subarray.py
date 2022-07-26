0e19da46880781655bc6cb67052789871bd59760
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-26 12:01:31 +0530

    Time: 261 ms (74.93%), Space: 15.2 MB (53.29%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -11,10 +11,11 @@ class Solution:
         for k in range(i, j+1):
             min_num = min(min_num, nums[k])
             max_num = max(max_num, nums[k])
-        while i > 0 and nums[i-1] > min_num:
+        while i>0 and nums[i-1] > min_num:
             i -= 1
-        while j < len(nums)-1 and nums[j+1] < max_num:
+        while j<len(nums)-1 and nums[j+1] < max_num:
             j += 1
         return j-i+1
         
+            
         
