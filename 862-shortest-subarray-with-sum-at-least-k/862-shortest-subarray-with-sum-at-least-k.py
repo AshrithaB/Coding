c77ea4d7b36332fdff9a916b65ed1ce72fc90d63
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-29 11:27:51 +0530

    Time: 2278 ms (31.06%), Space: 28.8 MB (12.04%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
@@ -1,20 +1,20 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        minLen = float("infinity")
-        q = collections.deque()
+        minLen = float('infinity')
         curSum = 0
+        q = collections.deque()
         l = 0
         for r in range(len(nums)):
             curSum += nums[r]
             if curSum >= k:
                 minLen = min(minLen, r+1)
-            curr = -1
+            cur = -1
             while q and curSum - q[0][0] >= k:
-                curr = q[0]
+                cur = q[0]
                 q.popleft()
-            if curr != -1:
-                minLen = min(minLen, r-curr[1])
+            if cur != -1:
+                minLen = min(minLen, r-cur[1])
             while q and curSum < q[-1][0]:
                 q.pop()
-            q.append([curSum,r]) 
-        return minLen if minLen != float("infinity") else -1
+            q.append([curSum, r])
+        return minLen if minLen != float('infinity') else -1
