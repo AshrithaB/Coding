c4daac7febca120eee5045ad36619298525ba7e7
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2022-07-29 11:56:27 +0530

    Time: 133 ms (67.85%), Space: 14.2 MB (6.68%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -1,20 +1,18 @@
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
         mod = 1000000007
-        res = 0
         hashMap = {}
+        count = 0
         for ch in arr:
             hashMap[ch] = 1 + hashMap.get(ch, 0)
         for i in hashMap:
             for j in hashMap:
                 k = target - i - j
-                if k not in hashMap:
-                    continue
-                if i==j and j==k:
-                    res += (hashMap[i] * (hashMap[i]-1) * (hashMap[i]-2)) // 6
-                elif i==j and j!=k:
-                    res += ((hashMap[i] * (hashMap[i]-1)) // 2) * hashMap[k]
-                elif i<j and j<k:
-                    res += hashMap[i] * hashMap[j] * hashMap[k] 
-        return res%mod
-        
+                if k not in hashMap: continue
+                if i == j and j == k:
+                    count += (hashMap[i] * (hashMap[j]-1) * (hashMap[k]-2))//6
+                elif i == j and j != k:
+                    count += ((hashMap[i] * (hashMap[j]-1))//2) * hashMap[k]
+                elif i < j and j < k:
+                    count += hashMap[i] * hashMap[j] * hashMap[k]
+        return count%mod
