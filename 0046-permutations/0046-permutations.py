aa3f53ab13814aa12f02214843820830b7074488
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-06-28 11:08:04 -0700

    Time: 59 ms (36.84%), Space: 16.6 MB (11.43%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        if len(nums) == 1:
+            return [nums.copy()]
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            prems = self.permute(nums)
+            for p in prems:
+                p.append(n)
+                res.append(p)
+            nums.append(n)
+        
+        return res
