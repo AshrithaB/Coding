188cf0b23d1ba0881b380ebca2637319b2ec8c81
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-07 13:26:40 -0700

    Time: 167 ms (5.8%), Space: 17.9 MB (13.48%) - LeetHub

---
+++ 0944-smallest-range-i/0944-smallest-range-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l, r = -k, k
+        res = float('inf')
+        while l<=r:
+            res = min(res, (nums[-1] + l) - (nums[0] + r))
+            l += 1
+            r -= 1
+        return res if res >= 0 else 0
