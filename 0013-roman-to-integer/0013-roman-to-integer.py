6343cf5393c2e332972a193d9063da5bd2525212
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-07 14:29:26 -0700

    Time: 59 ms (80.21%), Space: 16.3 MB (85.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,10 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         symbol = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        special = {'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
         num = 0
         for i in range(len(s)):
-            if i > 0 and s[i-1:i+1] in special:
-                num -= (symbol[s[i-1]]+symbol[s[i-1]])
-            num += symbol[s[i]]
+            if i+1 < len(s) and symbol[s[i]] < symbol[s[i+1]]:
+                num -= symbol[s[i]]
+            else:
+                num += symbol[s[i]]
         return num
