0e96a28ad409cf7fc9e13bcb1522af0dc0d349a0
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-10 13:18:30 -0700

    Time: 163 ms (5.68%), Space: 17.8 MB (13.35%) - LeetHub

--- 0944-smallest-range-i/0944-smallest-range-i.py
+++ 0944-smallest-range-i/0944-smallest-range-i.py
@@ -7,4 +7,5 @@ class Solution:
             res = min(res, (nums[-1] + l) - (nums[0] + r))
             l += 1
             r -= 1
-        return res if res >= 0 else 0
+        return res if res > 0 else 0
+    
