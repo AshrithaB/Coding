d0c8b001b614734d68aa472bfa3975b1f3a93f7e
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-10 11:27:23 -0700

    Time: 76 ms (96.57%), Space: 18.5 MB (56.5%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.py
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        if ops == []:
-            return m*n
         res = [m, n]
         for r,c in ops:
             res[0] = min(res[0], r)
