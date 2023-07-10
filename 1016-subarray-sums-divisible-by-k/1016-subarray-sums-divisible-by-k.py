a3de23495e4656078b15619272d6a182ccc811c3
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-10 11:44:44 -0700

    Time: 316 ms (39.27%), Space: 20.8 MB (30.96%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -1,11 +1,13 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        prefixhash = {0:1}
-        subarray = 0
+        map = {0:1}
         cursum = 0
+        res = 0
         for n in nums:
             cursum += n
             div = cursum % k
-            subarray += prefixhash.get(div, 0)
-            prefixhash[div] = 1 + prefixhash.get(div, 0)
-        return subarray
+            if div in map:
+                res += map[div]
+            map[div] = 1 + map.get(div, 0)
+        return res
+            
