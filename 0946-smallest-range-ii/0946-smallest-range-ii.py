91e50cd559ad700507755b5f5810e8115155800f
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-11 08:49:22 -0700

    Time: 170 ms (62.72%), Space: 17.6 MB (94.3%) - LeetHub

--- 0946-smallest-range-ii/0946-smallest-range-ii.py
+++ 0946-smallest-range-ii/0946-smallest-range-ii.py
@@ -3,8 +3,7 @@ class Solution:
         nums.sort()
         res = nums[-1] - nums[0]
         for i in range(len(nums)-1):
-            j = i+1
             high = max(nums[-1]-k, nums[i]+k)
-            low = min(nums[0]+k, nums[j]-k)
-            res = min(res, high-low)
+            low = min(nums[0]+k, nums[i+1]-k)
+            res = min(res, high - low)
         return res
