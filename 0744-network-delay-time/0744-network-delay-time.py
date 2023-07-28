b13250db9da47ea5491294ce87542ae06471c7a2
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-07-28 13:11:38 -0700

    Time: 421 ms (99.82%), Space: 18.1 MB (98.78%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -4,6 +4,8 @@ class Solution:
         adj = defaultdict(list)
         for u,v,w in times:
             adj[u].append([w,v])
+        if k not in adj:
+            return -1
         visit = set()
         minHeap = [[0,k]]
         while minHeap:
