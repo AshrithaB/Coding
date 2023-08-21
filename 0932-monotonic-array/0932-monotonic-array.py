0dcc53e8617f72fb6cb94c256b1c36ec9ccfdded
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-08-21 13:04:48 -0700

    Time: 890 ms (63.17%), Space: 30.6 MB (41.86%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -1,32 +1,10 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        else:
-            target = ""
-            if nums[0] < nums[1]:
-                target = "inc"  
-            elif nums[0] > nums[1]:
-                target = "dec"
-            if target == "inc":
-                i = 1
-                while i < len(nums) and nums[i-1] <= nums[i]:
-                    i += 1
-            elif target == "dec":
-                i = 1
-                while i < len(nums) and nums[i-1] >= nums[i]:
-                    i += 1
-            else:
-                i = 1
-                while i < len(nums) and nums[i-1] == nums[i]:
-                    i += 1
-                if i == len(nums): 
-                    return True
-                target = "inc" if nums[i-1] < nums[i] else "dec"
-                if target == "inc":
-                    while i < len(nums) and nums[i-1] <= nums[i]:
-                        i += 1
-                elif target == "dec":
-                    while i < len(nums) and nums[i-1] >= nums[i]:
-                        i += 1
-        return True if i == len(nums) else False
+        inc = True
+        dec = True
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                inc = False
+            if nums[i] < nums[i+1]:
+                dec = False
+        return inc or dec
