7d9cb65512800869b7a1a70b4491f5e370130f86
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-08-28 13:33:30 -0700

    Time: 123 ms (24.69%), Space: 20.9 MB (37.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        hashmap = defaultdict(list)
         for s in strs:
-            count = [0]*26
+            code = [0]*26
             for ch in s:
-                count[ord(ch) - ord('a')] += 1
-            count = str(count)
-            res[count].append(s)
-        return list(res.values())
-        
-
+                code[ord(ch) - ord('a')] += 1
+            hashmap[str(code)].append(s)
+        return hashmap.values()
