3336ff126099c464fb25e91eaba188bdf4026220
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-09-13 20:29:46 -0700

    Time: 268 ms (90.03%), Space: 18.9 MB (65.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        visit = set()
+        def dfs(i,j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            return
         island = 0
-
-        def bfs(r, c):
-            q = deque()
-            visit.add((r, c))
-            q.append((r, c))
-            while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-                for dr, dc in directions:
-                    r, c = row+dr, col+dc
-                    if (r in range(rows) and c in range(cols) and
-                        grid[r][c] == "1" and (r, c) not in  visit):
-                        q.append((r, c))
-                        visit.add((r,c))
-
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
+                if grid[r][c] == "1":
                     island += 1
-                    bfs(r, c)
+                    dfs(r,c)
         return island
