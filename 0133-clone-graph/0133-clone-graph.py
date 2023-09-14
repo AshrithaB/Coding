ef0fbd112affbc7939d05fef876c5d6467d40161
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-09-14 16:25:26 -0700

    Time: 42 ms (78.55%), Space: 16.6 MB (88.56%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -4,21 +4,22 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-        """
+"""
 
+from typing import Optional
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':   
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        oldtocopy = {}
+        newnodedict = {}
         def dfs(curr):
-            if curr in oldtocopy:
-                return oldtocopy[curr]
-            copy = Node(curr.val)
-            oldtocopy[curr] = copy
-            for n in curr.neighbors:
-                copy.neighbors.append(dfs(n))
-            return copy
-        return dfs(node)
-
+            if curr in newnodedict:
+                return newnodedict[curr]
+            newnode = Node(curr.val)
+            newnodedict[curr] = newnode
+            for nei in curr.neighbors:
+                newnode.neighbors.append(dfs(nei))
+            return newnode
+        dfs(node)
+        return newnodedict[node]
         
