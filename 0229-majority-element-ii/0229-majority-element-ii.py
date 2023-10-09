76b9add1ba94570762896796554e5c008a8cc367
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-10-09 00:13:58 -0700

    Time: 106 ms (78.83%), Space: 17.7 MB (30.37%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         l = math.floor(len(nums)/3)
-        if l == 0:
-            return list(set(nums))
-        else:
-            res = set()
-            count = {}
-            for n in nums:
-                count[n] = 1 + count.get(n, 0)
-                if count[n] > l:
-                    res.add(n)
-            return list(res)
+        res = set()
+        count = {}
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            if count[n] > l:
+                res.add(n)
+        return list(res)
 
             
