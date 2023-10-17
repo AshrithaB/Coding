ed4ae90650ff6ac97d4719de73150112890268e1
Ashritha <47385051+AshrithaB@users.noreply.github.com>
2023-10-16 17:27:00 -0700

    Time: 361 ms (10.74%), Space: 22.8 MB (6.63%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        new_inter = []
+        for i, inter in enumerate(intervals):
+            new_inter.append([inter,i])
+        new_inter.sort()
+        res = [-1 for _ in range(len(intervals))]
+        print(new_inter)
+        for i, inter in enumerate(new_inter):
+            end = inter[0][1]
+            s, e = i, len(new_inter)-1
+            print(end, s, e)
+            while s <= e:
+                m = (s+e)//2
+                if new_inter[m][0][0] >= end:
+                    e = m - 1
+                else:
+                    s = m + 1
+            if -1 < s < len(new_inter):
+                print(new_inter[s][1])
+                res[new_inter[i][1]] = new_inter[s][1]
+        return res
